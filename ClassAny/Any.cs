e7c2fd6f37778e3e910f038bbb4a5e4fb1542cd9
Nicorici <nickorici@yahoo.com>
2019-01-16 12:24:11 +0200

    Add project files.

---
+++ ClassAny/Any.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAny
+{
+    class Any
+    {
+        private string accepted;
+        private Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+        public 
+    }
+}
